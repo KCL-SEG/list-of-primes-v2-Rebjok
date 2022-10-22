91b71735f080e72fd6e633d5c085e3760d9b2d10
Rebi.boy <rebjok911@gmail.com>
2022-10-22 15:58:02 +0100

    Final solution

--- primes.py
+++ primes.py
@@ -1,6 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
+
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    track = 1
+    number = 3
+
+    if number_of_primes <= 0:
+        raise ValueError("Invalid Value: Please enter a number greater or equal to one.")
+
+
+    while track < number_of_primes:
+
+        is_prime = True
+        for i in range(3, int(math.sqrt(number) + 1), 2):
+            if number % i == 0:
+                is_prime = False
+
+        if is_prime:
+            list.append(number)
+            track += 1
+
+        number += 2
     return list
